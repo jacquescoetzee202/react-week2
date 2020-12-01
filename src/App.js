db033051c274cb60e08263aa8f2b8e7ee6e8b3a4
Jacques <jacquescoetzee91@gmail.com>
2020-12-01 11:17:56 +0000

    added form component and included in app with passing up data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import SignUp from "./lifting-state/SignUp";
 import Squares from "./lifting-state/Squares";
 import Button from "./passing-up-data/Button";
+import Form from "./passing-up-data/Form";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Squares color="hotpink" />
       <SignUp minimumLength={ 5 }/>
       <Button handleUpdate={ (value) => console.log(value) } />
+      <Form handleSubmit={ (value) => console.log(value) } />
     </div>
   );
 }
